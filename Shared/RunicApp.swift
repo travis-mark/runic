26a0cae5564ba2fe5d96e295baf198658592e7c5
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2021-03-01 19:03:27 -0500

    Initial Commit

---
+++ Shared/RunicApp.swift
@@ -0,0 +1,20 @@
+//
+//  RunicApp.swift
+//  Shared
+//
+//  Created by Travis Luckenbaugh on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RunicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
